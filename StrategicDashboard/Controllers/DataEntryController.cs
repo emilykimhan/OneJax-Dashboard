f51dd6b67199c79b0cfd2e198c8079f719ac9f89
Karrie Dominguez <karriedominguez09@gmail.com>
2025-10-01 23:20:06 -0400

    I put a simple data entry form in place.  It is not hooked up to anything yet. And it gives an error message (user story)

---
+++ StrategicDashboard/Controllers/DataEntryController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using OneJax_Dashboard.Models;
+
+namespace OneJax_Dashboard.Controllers
+{
+    public class DataEntryController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new EventEntryViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(EventEntryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Save to database
+                TempData["SuccessMessage"] = "Event successfully submitted!";
+                return RedirectToAction("Index");
+            }
+
+            // If validation fails, redisplay form with errors
+            return View(model);
+        }
+    }
+}
