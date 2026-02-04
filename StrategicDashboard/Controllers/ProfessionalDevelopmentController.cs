4544c05e0cedc891849d2ae0ff156c172d5b2f96
Karrie Dominguez <karriedominguez09@gmail.com>
2026-02-04 00:11:32 -0500

    Refactor GetStaffMembers method to retrieve staff data from the database

--- StrategicDashboard/Controllers/ProfessionalDevelopmentController.cs
+++ StrategicDashboard/Controllers/ProfessionalDevelopmentController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using OneJaxDashboard.Models;
 using OneJaxDashboard.Data;
 //karrie's
@@ -46,18 +47,15 @@ namespace OneJaxDashboard.Controllers
         }
 
 
-//Replace GetStaffMembers with the staff database, when all the staff accounts have been made or started
         private List<SelectListItem> GetStaffMembers()
         {
-            return new List<SelectListItem>
-            {
-                new SelectListItem { Value = "Elizabeth Andersen", Text = "Elizabeth Andersen" },
-                new SelectListItem { Value = "Cilicia Anderson", Text = "Cilicia Anderson" },
-                new SelectListItem { Value = "Jacey Kelly", Text = "Jacey Kelly" },
-                new SelectListItem { Value = "Deidre Lane", Text = "Deidre Lane" },
-                new SelectListItem { Value = "Jan Phillips", Text = "Jan Phillips" },
-                new SelectListItem { Value = "Rebekah Hutton", Text = "Rebekah Hutton" }
-            };
+            return _context.Staffauth
+                .Select(s => new SelectListItem 
+                { 
+                    Value = s.Name, 
+                    Text = s.Name 
+                })
+                .ToList();
         }
     }
 }
