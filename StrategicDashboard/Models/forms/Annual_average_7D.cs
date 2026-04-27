54b5ad99ffc2aa490fc4ceddc37a2d0c17ba1f4d
Karrie Dominguez <karriedominguez09@gmail.com>
2026-04-27 00:22:05 -0400

    feat: Add decimal precision to financial properties in Annual_average_7D and BudgetTracking_32D models

--- StrategicDashboard/Models/forms/Annual_average_7D.cs
+++ StrategicDashboard/Models/forms/Annual_average_7D.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 //Karrie's 
 namespace OneJaxDashboard.Models
 {
@@ -19,6 +20,7 @@ namespace OneJaxDashboard.Models
         [Required(ErrorMessage = "Percentage is required")]
         [Range(0, 100, ErrorMessage = "Percentage must be between 0 and 100")]
         [Display(Name = "Percentage of Respondents Identifying OneJax as Trusted Leader")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Percentage { get; set; }
  
         [Display(Name = "Total Respondents")]
